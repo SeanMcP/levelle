bdd0bd69f4aa285f9769a04b65f98f6ee3336553
Sean McPherson <git@seanmcp.com>
2020-02-15 07:12:25 -0500

    Read text from screen-captured images!

--- levelle.js
+++ levelle.js
@@ -1,55 +1,119 @@
-console.log('📖 Hello from Levelle!')
+(async () => {
+    console.log('📖 Hello from Levelle!')
 
-const video = document.getElementById('videoRef')
-const errors = document.getElementById('errorsRef')
+    let width = 320
+    let height
+    let isStreaming = false
 
-function logError(err) {
-    console.error(err)
-    errors.textContent = JSON.stringify(err, null, 2)
-}
+    const canvas = document.getElementById('canvasRef')
+    const photo = document.getElementById('photoRef')
+    const video = document.getElementById('videoRef')
+    const errors = document.getElementById('errorsRef')
 
-async function startStream() {
-    if (video && navigator.mediaDevices.getUserMedia) {
-        try {
-            const stream = await navigator.mediaDevices.getUserMedia({
-                video: true
-                // video: {
-                //     facingMode: { exact: 'environment' }
-                // }
-            })
-            video.srcObject = stream
-            readStream()
-        } catch (err) {
-            logError(err)
+    document.getElementById('startButton').addEventListener('click', startStream)
+    document.getElementById('stopButton').addEventListener('click', stopStream)
+    document.getElementById('pictureButton').addEventListener('click', takePicture)
+
+    function logError(err) {
+        console.error(err)
+        errors.textContent = JSON.stringify(err, null, 2)
+    }
+
+    async function startStream() {
+        if (video && navigator.mediaDevices.getUserMedia) {
+            try {
+                const stream = await navigator.mediaDevices.getUserMedia({
+                    video: true
+                    // video: {
+                    //     facingMode: { exact: 'environment' }
+                    // }
+                })
+                video.srcObject = stream
+                video.play()
+            } catch (err) {
+                logError(err)
+            }
         }
     }
-}
 
-function stopStream() {
-    if (video.srcObject) {
-        const tracks = video.srcObject.getTracks()
-
-        for (let i = 0; i < tracks.length; i++) {
-            tracks[i].stop()
+    video.addEventListener('canplay', () => {
+        if (!isStreaming) {
+            height = video.videoHeight / (video.videoWidth/width);
+            
+            video.setAttribute('width', width);
+            video.setAttribute('height', height);
+            canvas.setAttribute('width', width);
+            canvas.setAttribute('height', height);
+            isStreaming = true;
         }
+    })
 
-        video.srcObject = null
+    function stopStream() {
+        if (video.srcObject) {
+            const tracks = video.srcObject.getTracks()
+
+            for (let i = 0; i < tracks.length; i++) {
+                tracks[i].stop()
+            }
+
+            video.srcObject = null
+        }
     }
-}
 
-function readStream() {
-    if (window.Tesseract) {
-        const { TesseractWorker } = window.Tesseract
-        const worker = new TesseractWorker()
+    async function readStream() {
+        if (window.Tesseract) {
+            const { createWorker } = window.Tesseract
+            const worker = createWorker({
+                logger: message => console.log(message)
+            })
 
-        worker
-            .recognize(video)
-            .progress(progress => {
-                console.log('progress', progress)
-            })
-            .then(result => {
-                console.clear()
-                console.log('result', result)
-            })
+            await worker.load();
+            await worker.loadLanguage('eng');
+            await worker.initialize('eng');
+            const { data: { text } } = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
+            console.log(text);
+            await worker.terminate();
+        }
     }
-}
+    
+    async function readPhoto(image) {
+        if (window.Tesseract) {
+            const { createWorker } = window.Tesseract
+            const worker = createWorker({
+                logger: message => console.log(message)
+            })
+    
+            await worker.load();
+            await worker.loadLanguage('eng');
+            await worker.initialize('eng');
+            const { data: { text } } = await worker.recognize(image);
+            console.log(text);
+            await worker.terminate();
+        }
+    }
+
+    function takePicture() {
+        const context = canvas.getContext('2d');
+        if (width && height) {
+            canvas.width = width;
+            canvas.height = height;
+            context.drawImage(video, 0, 0, width, height);
+            
+            const data = canvas.toDataURL('image/png');
+            photo.setAttribute('src', data);
+            readPhoto(data)
+            
+        } else {
+            clearPicture()
+        }
+    }
+
+    function clearPicture() {
+        const context = canvas.getContext('2d');
+        context.fillStyle = "#AAA";
+        context.fillRect(0, 0, canvas.width, canvas.height);
+    
+        const data = canvas.toDataURL('image/png');
+        photo.setAttribute('src', data);
+      }
+})()
